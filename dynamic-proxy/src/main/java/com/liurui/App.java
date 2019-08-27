91f8343aa557de556533c4736c9937e6d3ea6225
liurui <liurui@daling.com>
2019-08-27 19:57:20 +0800

    初始化代码

--- dynamic-proxy/src/main/java/com/liurui/App.java
+++ dynamic-proxy/src/main/java/com/liurui/App.java
@@ -1,13 +1,43 @@
 package com.liurui;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public enum A {
+        AA(1),
+        BB(2),
+        ;
+
+        public static final Map<Integer, A> Maps;
+
+        static {
+            Maps = new HashMap<>();
+
+            for (A value : A.values()) {
+                Maps.put(value.getCode(), value);
+            }
+        }
+
+        private int code;
+
+        A(int code) {
+            this.code = code;
+        }
+
+
+        public int getCode() {
+            return code;
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+
     }
 }
